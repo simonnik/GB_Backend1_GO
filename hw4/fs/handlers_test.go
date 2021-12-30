e386045563a3a0ae62b2c43b0073b0c43255cd99
simon <trishichkin@mail.ru>
2021-12-30 23:09:49 +0300

    HW4 - Реализация и тестирование веб-сервера
    Запуск: cd hw4/fs && go run server.go handlers.go
    Загрузка файла: curl 'localhost:8089/upload' -X POST  -F 'file=@testfile.txt'
    Получить список файлов: curl 'localhost:8089/list?ext=.txt'

--- hw4/fs/handlers_test.go
+++ hw4/fs/handlers_test.go
@@ -2,11 +2,58 @@ package main
 
 import (
 	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 )
+
+func TestUploadHandlerMethodNotAllowed(t *testing.T) {
+	body := &bytes.Buffer{}
+	req, _ := http.NewRequest(http.MethodGet, "/upload", body)
+	rr := httptest.NewRecorder()
+	uploadHandler(rr, req)
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+	}
+}
+
+func TestUploadHandlerUnableReadFile(t *testing.T) {
+	body := &bytes.Buffer{}
+	req, _ := http.NewRequest(http.MethodPost, "/upload", body)
+	rr := httptest.NewRecorder()
+	uploadHandler(rr, req)
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+	}
+}
+
+func TestUploadHandler(t *testing.T) {
+	expected := `testfile.txt`
+	file, _ := os.Open(expected)
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	req, _ := http.NewRequest(http.MethodPost, "/upload", body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	rr := httptest.NewRecorder()
+
+	uploadHandler(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+}
 
 func TestListHandler(t *testing.T) {
 	body := &bytes.Buffer{}
